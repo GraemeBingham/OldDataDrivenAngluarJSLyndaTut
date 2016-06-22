da34882353f7f22434a2698427d3fa36418941f2
GraemeBingham <binghamgraeme@gmail.com>
2016-06-22 19:18:12 +1000

    Added firebase for adding meettings

--- builds/development/js/controllers/meetings.js
+++ builds/development/js/controllers/meetings.js
@@ -1,8 +1,17 @@
 myApp.controller('MeetingsController',
-  function ($scope, $firebaseObject) {
+  function ($firebase, $scope, $firebaseObject, $firebaseArray) {
+
   var ref = new Firebase("https://attendanceldcapp-85101.firebaseio.com/meetings");
+  var meetings =  $firebaseArray(ref);
 
   $scope.meetings = $firebaseObject(ref);
 
-
+  $scope.addMeeting=function() {
+    meetings.$add({
+      name: $scope.meetingname,
+      date: Firebase.ServerValue.TIMESTAMP
+    }).then(function() {
+      $scope.meetingname = '';
+    });
+  }
 });
